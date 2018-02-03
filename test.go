01a05271e4a8e1dd3fe94aa4a999724a69d3256f
thienng93 <thiennng93@gmail.com>
2018-02-03 14:59:15 -0800

    first commit

--- test.go
+++ test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,8 +10,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<body><h1>Hello world!</h1></body>")
 }
 
+func loadPage(page string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		filename := "./view/" + page
+		body, err := ioutil.ReadFile(filename)
+		if err != nil {
+			panic(err)
+		}
+		w.Write(body)
+	}
+}
+
 func main() {
+	http.HandleFunc("/login", loadPage("login.html"))
 	http.HandleFunc("/", handler)
+	//http.HandleFunc("/intro/", loadPage("login.ht"))
 
 	http.ListenAndServe(":8080", nil)
 }
